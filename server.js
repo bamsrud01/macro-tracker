5a17b5c85dff69eca6418378be643e13e239d6d9
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-13 00:15:15 -0500

    Continued with express

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 //  Declare node modules
 const express = require('express');
-
+const path = require('path');
+const bodyParser = require('body-parser');
 
 //  Initialize project
 let app = express();
 
+//  Declare middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//  Create routes
+
+//  Connect routes
+
+//  Set up public connection
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
 //  Declare port variable
-let port = 5000;
+let port = process.env.PORT || 5000;
 
 //  Start express server
-app.listen(port);
-console.log('Listening on port ' + port);
+let server = app.listen(port, function() {
+  console.log('Listening on port ' + server.address().port);
+});
