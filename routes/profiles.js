a568ada3c76fb5a9e2c1dcc2faa61f28c90ae9d4
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-24 11:40:13 -0500

    Set up database connections

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,5 +3,16 @@
 //  Declare file as a router
 const router = require('express').Router();
 
+//  Import PostgreSQL
+const pg = require('pg');
+
+//  Use 'macrotrack' database
+let config = {
+  database: 'macrotrack'
+}
+
+//  Initialize connection pool
+let pool = new pg.Pool(config);
+
 //  Export router
 module.exports = router;
