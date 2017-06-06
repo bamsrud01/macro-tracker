313a4020f3db4e6a427e5e096ea4056ee6236277
Barrett Amsrud <barrettamsrud@gmail.com>
2017-06-05 21:48:30 -0500

    Added registration control

--- public/scripts/register.controller.js
+++ public/scripts/register.controller.js
@@ -17,21 +17,23 @@ function RegisterController(RegisterService) {
   register.restrictions = {
     existingUsername: false,
     validEmail: false,
+    matchingPassword: true
   }
 
   //  Checks if user has completed all registration requirements
-  register.failure = false;
+  register.failure = true;
   register.confirm = '';
 
   //  Ensure email is valid
   register.validateEmail = function() {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(register.email)) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(register.user.email)) {
       console.log('Email is valid');
       register.restrictions.validEmail = true;
     } else {
       console.log('Email is NOT valid.');
       register.restrictions.validEmail = false;
     }
+    register.checkRequirements();
   };
 
   //  Set initial Calorie goal from given information (can be customized)
@@ -47,10 +49,43 @@ function RegisterController(RegisterService) {
     if (register.user.fat) {
       goal += (register.user.fat * 9);
     }
-    console.log('Goal:', goal);
+
     if (goal != 0) {
       register.user.calories = goal;
     }
+  }
+
+  // Check username unique status
+  register.uniqueUsername = () => {
+    //  User Service to check table
+  }
+
+  //  Check password requirements
+  register.comparePassword = () => {
+    if ((register.user.password.length >= 8) && (register.user.password != register.confirm)) {
+      register.restrictions.matchingPassword = false;
+      console.log('Not a match');
+    } else {
+      register.restrictions.matchingPassword = true;
+      console.log('Match');
+    }
+    // Function to check requirements
+    register.checkRequirements();
+  }
+
+  //  Check all profile requirements
+  register.checkRequirements = () => {
+    let { existingUsername, validEmail, matchingPassword } = register.restrictions;
+    if (existingUsername && validEmail && matchingPassword) {
+      register.failure = false;
+    } else {
+      register.failure = true;
+    }
+  }
+
+  //  Submit user information
+  register.submitProfile = () => {
+    console.log(register.user);
   }
 
 
