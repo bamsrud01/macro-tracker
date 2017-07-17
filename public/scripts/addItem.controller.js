5848c70119e5baff535058be2ae00b0dfb6a7e79
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-16 21:08:46 -0500

    Added structure

--- public/scripts/addItem.controller.js
+++ public/scripts/addItem.controller.js
@@ -6,4 +6,15 @@ function AddItemController(AddItemService) {
 
   let add = this;
 
+  //  Starting object (take from nav depending on food or recipe?)
+  //  const defaults = {};
+
+  //  Foods
+  //  { name, variety, brand, serving, calories, carbs, fiber, protein, fat }
+  //  Add user_id from active
+
+  //  Recipes
+  //  { name, serving, calories, carbs, fiber, protein, fat, directions, source, source_url }
+  //  Add [ food_id ] for ingredients
+
 }
