50490f00436afbb93018beec6d18e2742d4f5137
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-26 12:03:08 -0500

    Added default user for development

--- public/scripts/app.module.js
+++ public/scripts/app.module.js
@@ -4,8 +4,19 @@ angular.module('macrotrack')
   .controller('MainController', MainController);
 
 //  Parent of all controllers.  Uses MainService to move data between pages
+//  Handles login on the main navbar
 function MainController(MainService) {
 
   let main = this;
+
+
+  //  Global state of app
+  //  TEMPORARY STATE:  { logginIn: true, username: TESTUSER, user_id: 1000 }
+  //  Reset to { loggedIn: false, username: false, user_id: false }
+  main.state = {
+    loggedIn: true,
+    username: 'TESTUSER',
+    user_id: 1000
+  }
 
 }
