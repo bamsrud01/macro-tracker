8cd55544362c309d354cab45cb4420ad1a313fc7
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-04 23:13:32 -0500

    Can add recipes

--- public/scripts/addrecipe.controller.js
+++ public/scripts/addrecipe.controller.js
@@ -2,9 +2,63 @@ angular.module('macrotrack')
   .controller('AddRecipeController', AddRecipeController);
 
 //  Manages CRUD operations on recipes
-function AddRecipeController(AddRecipeService) {
+function AddRecipeController(AddRecipeService, MainService, $location) {
 
   let add = this;
+
+  //  Contains add.newRecipe {recipe_data}
+  //  Contains add.ingredientsList [{ingredient_data}]
+  add.ingredientsList = ['DUMMY DATA'];
+
+  // If an existing recipe, populate fields
+  if (AddRecipeService.existingRecipe) {
+    add.newRecipe = AddRecipeService.recipeToEdit;
+    AddRecipeService.existingRecipe = false;
+  }
+
+  //  Set mode to display buttons for Add, or for Update and Delete
+  if (add.newRecipe) {
+    add.editMode = true;
+  } else {
+    add.editMode = false;
+  }
+
+  //  Recipe with all fields at null
+  const emptyRecipe = {
+    name: null,
+    serving: null,
+    calories: null,
+    carbs: null,
+    fiber: null,
+    protein: null,
+    fat: null,
+    directions: null,
+    source: null,
+    source_url: null,
+    user_id: null
+  };
+
+  //  Function to submit a new recipe
+  //  NOTE: Submit to recipes and foods_recipes
+  add.submitNewRecipe = () => {
+    if (MainService.state.loggedIn && add.newRecipe.name && add.newRecipe.directions && add.ingredientsList.length > 0) {
+      add.newRecipe.user_id = MainService.state.user_id;
+      AddRecipeService.submitNewRecipe(add.newRecipe).then(response => {
+        console.log('Success!  New recipe created:', response);
+        //  Post ingredients
+        add.newRecipe = emptyRecipe;
+      });
+
+      // postIngredients();
+    } else {
+      console.log('Error!  Check required fields');
+    }
+  }
+
+  //  Function to post ingredients array data
+  function postIngredients() {
+
+  }
 
   console.log('On Add Recipe');
 
--- public/scripts/addrecipe.service.js
+++ public/scripts/addrecipe.service.js
@@ -4,5 +4,22 @@ angular.module('macrotrack')
 function AddRecipeService($http) {
 
   let service = this;
-  
+
+  //  Will contain:
+    //  existingRecipe {false}
+    //  recipeToEdit {recipe_object}
+
+  //  Post a new food to the database
+  service.submitNewRecipe = (recipeData) => {
+    console.log('Will POST data:', recipeData);
+    return $http({
+      method: 'POST',
+      url: '/recipe',
+      data: recipeData
+    }).then(response => {
+      console.log('SERVICE received response:', response);
+      return response.data;
+    });
+  }
+
 }
