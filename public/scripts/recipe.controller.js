2e3e86d81a5bddf86a9b13ac5a76237cbd5a4cec
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-06 19:47:31 -0500

    Added button to edit recipes in recipe list

--- public/scripts/recipe.controller.js
+++ public/scripts/recipe.controller.js
@@ -2,15 +2,25 @@ angular.module('macrotrack')
   .controller('RecipeController', RecipeController);
 
 //  Manages displaying and sorting existing recipe objects
-function RecipeController(RecipeService) {
+function RecipeController(MainService, RecipeService, AddRecipeService, $location) {
 
   let recipe = this;
+
+  //  { loggedIn, username, user_id }
+  recipe.activeUser = MainService.state;
 
   //  Function to get all recipes in the database
   recipe.getAllRecipes = () => {
     RecipeService.getAllRecipes().then(recipeData => {
       recipe.listedRecipes = recipeData;
     });
+  }
+
+  //  Function to edit food if same user_id
+  recipe.editRecipe = (recipeObject) => {
+    AddRecipeService.existingRecipe = true;
+    AddRecipeService.recipeToEdit = recipeObject;
+    $location.path('/add-recipe');
   }
 
   //  Get all recipes in database when opening view
