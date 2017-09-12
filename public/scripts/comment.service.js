484f8b602156ea74b658c488e3097ca3a6ee0457
Barrett Amsrud <barrettamsrud@gmail.com>
2017-09-11 22:50:49 -0500

    Added get and delete function for comments

--- public/scripts/comment.service.js
+++ public/scripts/comment.service.js
@@ -7,17 +7,39 @@ function CommentService($http) {
 
   let service = this;
 
-  //  CREATE: commentType, userId, resourceId, commentDate, comment
-  //  READ: commentType, resourceId
-  //  UPDATE: commentType, id, resourceId, commentDate, comment
-  //  DELETE: commentType, id
+  //  TEST CREATE: commentType, userId, resourceId, commentDate, comment
+  //  TEST READ: commentType, resourceId
+  //  TEST UPDATE: commentType, id, resourceId, commentDate, comment
+  //  TEST DELETE: commentType, commentId
 
-  service.getComments
+  //  commentType = 'food || recipe || profile'
+  //  resourceId = foodId || recipeId || userId
+
+  //  Get all comments
+  service.getComments = (commentType, resourceId) => {
+    return $http.get('/comment/' + commentType, {
+      params: {
+        id: resourceId
+      }
+    }).then(response => {
+      return response.data;
+    });
+  }
 
   service.postComment
 
   service.updateComment
 
-  service.deleteComment
+  //  Delete a comment
+  service.deleteComment = (commentType, commentId) => {
+    return $http.delete('/comment/' + commentType, {
+      params: {
+        id: commentId
+      }
+    }).then(response => {
+      console.log('Comment deleted:', response);
+      return;
+    });
+  }
 
 }
