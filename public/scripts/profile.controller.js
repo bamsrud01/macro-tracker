4b531686a8250918699f610ef763560a68dd4e36
Barrett Amsrud <barrettamsrud@gmail.com>
2017-09-16 23:51:00 -0500

    Continued planning profile

--- public/scripts/profile.controller.js
+++ public/scripts/profile.controller.js
@@ -9,6 +9,6 @@ function ProfileController(ProfileService) {
   //  GOALS
     //  Lots of useful data visualization here:  Weight progress, macronutrient distribution, logged date rows, etc.
 
-  console.log('On Profile');
+  //  Store user data in profile.selectedUser
 
 }
